35b137d8a82461eac2213a1bb85c43de5d7f1293
CCrosbyweb <celeste@eagles.ewu.edu>
2014-05-12 14:24:56 -0700

    version fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
 // My Scripts
 $(document).foundation();
+	$('article').readmore({
+		
+		maxHeight : 0,
+		
+	});
+// Click link hide article
+$('a').click(function(){
+	$('article').readmore({
+		speed : 1000,
+		maxHeight : 0,
+		startOpen: true
+	});
+
+});
+
